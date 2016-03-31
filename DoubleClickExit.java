b0a13cbfbbc8570fb877306b1480a67c4e33916d
GrogramCat <temoa_mic@hotmail.com>
2016-03-31 17:22:37 +0800

    update

---
+++ DoubleClickExit.java
@@ -0,0 +1,17 @@
+﻿private long exitTime = 0;  
+@Override  
+public boolean onKeyDown(int keyCode, KeyEvent event) {  
+    if (keyCode == KeyEvent.KEYCODE_BACK  
+            && event.getAction() == KeyEvent.ACTION_DOWN) {  
+        if ((System.currentTimeMillis() - exitTime) > 2000) {  
+            Toast.makeText(getApplicationContext(), "再按一下退出应用",  
+                    Toast.LENGTH_SHORT).show();  
+            exitTime = System.currentTimeMillis();  
+        } else {  
+            finish();  
+            System.exit(0);  
+        }  
+        return true;  
+    }  
+    return super.onKeyDown(keyCode, event);  
+} 
