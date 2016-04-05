bb8bc2aafbff08fd249620c4fa67ce4b74fda8c6
GrogramCat <temoa_mic@hotmail.com>
2016-04-05 21:56:25 +0800

    update

---
+++ customView&wh.java
@@ -0,0 +1,20 @@
+﻿protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+	int w = customMeasure(widthMeasureSpec);
+	int h = customMeasure(heightMeasureSpec);
+	setMeasuredDimension(w, h);
+}
+
+private int customMeasure(int measureSpec) {
+	int result = 0;
+	int specMode = MeasureSpec.getMode(measureSpec);
+	int specSize = MeasureSpec.getSize(measureSpec);
+	if (specMode == MeasureSpec.EXACTLY) {
+		result = specSize;
+	} else {
+		result = 200;
+		if (specMode == MeasureSpec.AT_MOST) {
+			result = Math.min(result, specSize);
+		}
+	}
+	return result;
+}
