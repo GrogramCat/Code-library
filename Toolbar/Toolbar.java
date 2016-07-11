552ff3f82d7b3703244ba6880c8992660d30ac9d
GrogramCat <temoa_mic@hotmail.com>
2016-07-11 17:13:09 +0800

    update

---
+++ Toolbar/Toolbar.java
@@ -0,0 +1,80 @@
+// 在代码中
+@Override
+protected void onCreate(Bundle saveInstanceState){
+    super.onCreate(saveInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);// 继承AppCompatActivity
+
+    // 设置导航图标
+    mToolbar.setNavigationIcon(...);
+    // 设置标题
+    getSupportActionBar.setTitle(...);
+}
+
+// Activity的Theme设为NoActionBar；或者Theme.AppCompat并重写以下
+    // <item name="windowActionBar">false</item>
+    // <item name="android:windowNoTitle">true></item>
+
+// Toolbar支持设置单独一个Theme
+/*
+ *<android.support.v7.widget.Toolbar
+ *  android:id="@+id/toolbar"
+ *  ...
+ *  app:theme="@style/Theme.AppCompat"/>  
+ */
+// 然后给Activity设置一个Light主题
+
+
+// Toolbar的特技
+// 设置Toolbar的背景透明度
+mActionbarDrawable = new ColorDrawable(getResources().getColor(R.color.red));
+getSupportActionBar.setBackgroundDrawable(mActionbarDrawable);
+protected void setToolbarAlpha(float alpha) {
+    mActionbarDrawable.setAlpha((int)(alpha*255));
+}
+
+// 动画显示和隐藏Toolbar
+public void showToolbar(boolean show) {
+    if (show == toolbarShow || toolbar == null) return;
+    toolbarShow = show;
+    if (show) {
+        toolbar.animate()
+               .translationY(0)
+               .alpha(1)
+               .setDuration(300)
+               .setInterpolator(new DecelerateInterPolator());
+    } else {
+         toolbar.animate()
+               .translationY(-toolbar.getBottom())
+               .alpha(0)
+               .setDuration(300)
+               .setInterpolator(new DecelerateInterPolator());
+    }
+}
+
+// 配合RecylerView使用
+public abstract class HidingScrollListener extends RecyclerView.OnScrollListener {
+    @Override
+    public void OnScrolled(RecyclerView recyclerview, int dx, int dy) {
+        super.onScroiied(recyclerview, dx, dy);
+        if (dy>1) {
+            onHide();
+        } else if (dy<-1) {
+            onShow();
+        }   
+    }
+
+    public abstract void onHide();
+    public abstract void onShow();
+}
+
+RecyclerView.setOnScrollListener(new HidingScrollListener() {
+    @Override
+    public void onHide() {
+        showToolbar(false);
+    }
+    public void onShow() {
+        showToolbar(true);
+    }
+});
---
+++ 测试/Junit.java
@@ -0,0 +1,125 @@
+/*
+ JUnit ——是一个开发源代码的Java测试框架，用于编写和运行可重复的测试。
+ 他是用于单元测试框架体系xUnit的一个实例（用于java语言）。
+ 主要用于白盒测试，回归测试。
+ */
+
+
+// Junit 所有的测试方法都是以@Test 修饰，以public void 开头
+@Test
+public void textAdd() {
+    assertEquals(4, 2+2);
+}
+
+// @BeforeClass & @AfterClass 都只会执行一次
+// @BeforeClass在类加载时执行
+// @AfterClass在类加载结束时执行
+// 都为静态的方法
+@BeforeClass
+public static void setUpBeforeClass() throws Exception {
+    System.out.print("before class");
+}
+
+@AfterClass
+public static void tearDownAfterClass() throws Exception {
+    System.out.print("after class");
+}
+
+// @Before & @After 在每个测试方法执行时都会执行一次
+// @Before 在测试方法执行前，@After 在测试方法执行后
+@Before
+public void setUp() throws Exception {
+    System.out.print("before");
+}
+
+@After
+public void tearDown() throws Exception {
+    System.out.print("after");
+}
+
+// @Ignore 所修饰的测试方法会被测试运行器忽略，如下测试方法：
+@Ignore
+@Test
+public void text1() throws Exception {
+    System.out.print("test1");
+}
+
+// @Test(timeout = 毫秒)，用来指定时间上限
+// 如果测试方法执行的时间超过了指定的时间，则测试失败
+@Test(timeout = 1000)
+public void testTime() {
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
+
+// @Test(expected = 异常类)，用excepted来指定应该抛出的异常
+// 如果在测试中没有抛出异常或者抛出的异常与指定异常不同，则测试失败
+@Test(expected = InedxOutBoundException.class)
+public void textArrayList() throws Exception {
+    new ArrayList<String>().get(1);
+}
+
+// 测试套件就是组织所要测试的类一起运行，如果单个类单独的运行是比较麻烦的，可以使用测试套件一起运行这些测试类。
+// 需要注意的是：
+// 1测试套件的类是不包含其他任何方法
+// 2同时要更改测试运行器为Suite.class
+// 3将要测试的类作为数组传入到SuiteClasses({})中
+
+// 更改测试运行器以及将要测试的类放入SuiteClasses中
+  @RunWith(Suite.class)
+  @SuiteClasses({ AppTest.class, CalculateTest.class, JunitFlowTest.class })
+  public class AllTests {
+      // 没有测试方法
+  }
+
+
+// 栗子：
+public class CountNumber {
+    public boolean IfNumberIsPhoneNumber(String number) {
+        if (0 == number.length())
+            return false;
+        if (number.length() == 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class CountNumberTest {
+
+    private CountNumber countNumber;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        System.out.println("@AfterClass");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        countNumber = new CountNumber();
+        System.out.println("@before");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("@after");
+    }
+
+    @Test
+    public void testIfNumberIsPhoneNumber() throws Exception {
+        System.out.println("Now,testing...");
+        assertEquals(true, countNumber.IfNumberIsPhoneNumber("13590079433"));
+        assertEquals(false,countNumber.IfNumberIsPhoneNumber("23213"));
+        assertTrue(countNumber.IfNumberIsPhoneNumber("13652764553"));
+        assertFalse(countNumber.IfNumberIsPhoneNumber("23131"));
+    }
+}
